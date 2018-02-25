760cbea3fac3ead720addc71bb7dbfd4837d2d95
root <root@DESKTOP-J620AFP.localdomain>
2018-02-25 19:29:35 +0200

    websocket api page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get 'pages/api_tase_bridge'
   get 'pages/tradingmachine'
   get 'pages/strategies_api'
+  get 'pages/websocket_api'
   get 'pages/terms'
   get 'pages/privacy'
   root 'pages#index'
